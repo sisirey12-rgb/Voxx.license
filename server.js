413f9488e5c3eb54c653f3e59a520268af192e71
ANYASISIREY <sisirey12@gmail.com>
2026-07-29 00:13:10 +0530

    Update CORS configuration and add cookie parser

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const { init } = require('./db');
 const adminRoutes = require('./routes/admin');
@@ -8,7 +9,13 @@ const licenseRoutes = require('./routes/license');
 const resellerRoutes = require('./routes/reseller');
 
 const app = express();
-app.use(cors()); // tighten with { origin: 'https://your-netlify-site.netlify.app' } once deployed
+
+app.use(cors({
+  origin: 'https://YOUR-NETLIFY-SITE.netlify.app', // replace with your Netlify URL
+  credentials: true,
+}));
+
+app.use(cookieParser());
 app.use(express.json());
 
 app.get('/', (req, res) => {
